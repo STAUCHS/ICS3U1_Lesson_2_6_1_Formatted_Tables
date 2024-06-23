85db550b09e69c2a3021f814523b85b11be98d33
mschen-sta <cherrie.chen@ycdsb.ca>
2024-06-23 13:07:22 -0400

    Added lesson notes

--- KmTOMiles2.java
+++ KmTOMiles2.java
@@ -10,9 +10,13 @@ class KmTOMiles2 extends ConsoleProgram {
 
     double dblMiles;
 
+    // output table header
+    System.out.printf("%5s %10s %n", "Km", "Miles");
+    System.out.println("------------------");
+
     for (int intKm = 10; intKm <= 100; intKm += 10){
       dblMiles = intKm * 0.621371;
-      System.out.println(intKm + "km --> " + dblMiles + "mi");
+      System.out.printf("%5d %10.2f %n", intKm, dblMiles);
 
     }
   }
